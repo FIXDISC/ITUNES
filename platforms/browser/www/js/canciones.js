ffc4537c61c88adaf7a2937cef695c242ec3508d
Alejandro <acavieres@hotmail.com>
2019-11-08 20:23:09 -0300

    Console log de Array canciones

--- platforms/browser/www/js/canciones.js
+++ platforms/browser/www/js/canciones.js
@@ -8,15 +8,39 @@ fetch(url, {mode: 'cors'})
   .then(function(response) {
     response.text().then(function(text) {
       text = text.replace(/\s/g, '');
-      setTimeout(function(){muestra_canciones(JSON.parse(text));},3000);
+
+      setTimeout(function(){
+			muestra_canciones(JSON.parse(text));  
+		},3000);
     });
   });
 }
 
 
 function muestra_canciones(data){
-	load_p('canciones.html','contenedor',0);
-	alert(data.results[0].artworkUrl100);
-	setTimeout(function(){document.getElementById('foto').src = data.results[0].artworkUrl100;},1000);
+	canciones = [];
+	Object.keys(data).forEach(function(k){
+		//console.log(k + ' - ' + data[k]);
+		//canciones.push(data.results[0].artworkUrl100);
+		console.log(data[k]);
+			for(v=0;v<data[k].length;v++){
+				//console.log(data[k][v]);
+				//console.log("ART: "+data[k][v].artistName);
+				canciones.push("{"+data[k][v].artworkUrl100+","+data[k][v].collectionName+"}");
+			}
+	});
+	
+	console.log(canciones);
+	return;
+	
+	document.getElementById('con').style.display='none';
+	document.getElementById('rec').style.display='block';
+	setTimeout(function(){
+		load_p('canciones.html','contenedor',0);
+	},2000);	
+	
+	setTimeout(function(){
+		document.getElementById('foto').src = data.results[0].artworkUrl100;
+	},2100);
 	
 }
